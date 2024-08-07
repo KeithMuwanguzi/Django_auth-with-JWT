6d5dd8b84987bcdcfb40b20e38604a52a9abdfc2
keith <keithmuwanguzi@gmail.com>
2024-08-07 12:46:15 +0300

    2nd Commit

--- api/views.py
+++ api/views.py
@@ -2,11 +2,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from . serializers import UserSerializer
 
 @api_view(['POST'])
 def login(request):
-    return Response()
+    return Response({'Response':'Under development'})
 
-
+@api_view(['POST'])
 def signUp(request):
-    return Response()
+    ser = UserSerializer(data = request.data)
+    if ser.is_valid():
+        ser.save()
+        user = User.objects.get(username = request.data['username'])
+        token = Token.objects.create(user)
+        return Response({'Token':token,'user_data':ser.data})
+    else:
+        return Response({"Response":"An error occured"})
