30740107d6d0454d7bca985954c85b85b0af7a13
keith <keithmuwanguzi@gmail.com>
2024-08-08 10:51:04 +0300

    4th commit

--- api/views.py
+++ api/views.py
@@ -1,19 +1,58 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
-from . serializers import UserSerializer
+from . serializers import UserSerializer 
+from rest_framework.response import Response 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def login(request):
-    return Response({'Response':'Under development'})
+    user = get_object_or_404(User, username = request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({
+            'message':'The details you entered do not match an active user',
+            'status':'error'
+        })
+    else:
+        token, created = Token.objects.get_or_create(user=user)
+        serializer = UserSerializer(instance = user)
+        return Response({
+            'token':token.key,
+            'data':serializer.data
+        })
 
 @api_view(['POST'])
 def signUp(request):
-    ser = UserSerializer(data = request.data)
-    if ser.is_valid():
-        user = ser.save()
+    serializer = UserSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username = request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        serializer = UserSerializer(user)
         token, created = Token.objects.get_or_create(user=user)
-        return Response({'Token': token.key, 'user_data': ser.data}, status=201)
+        return Response({
+            'message':'User created successfully',
+            'data':{
+                'token':token.key,
+                'data':serializer.data
+            },
+            'status':'success'
+        })
     else:
-        return Response({"Response": "An error occurred", "errors": ser.errors}, status=400)
+        return Response({
+            'message':'An error occured',
+            'error':serializer.errors,
+            'status':'error'
+        })
+    
+
+@api_view(['GET'])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response({
+        'message':'Users fetched successfully',
+        'data': serializer.data,
+        'status':'success'
+    })
